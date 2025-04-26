71144301e8855408b0ca4029423d44bb023c2426
Luke Bollwerk <lukas8@gmail.com>
2025-04-26 11:13:26 -0600

    first commit

---
+++ app/src/commonMain/kotlin/ponder/contemplate/ui/ExampleProfileScreen.kt
@@ -0,0 +1,34 @@
+package ponder.contemplate.ui
+
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.runtime.*
+import androidx.lifecycle.viewmodel.compose.viewModel
+import ponder.contemplate.ExampleProfileRoute
+import pondui.ui.controls.Button
+import pondui.ui.controls.Controls
+import pondui.ui.controls.Text
+import pondui.ui.controls.TextField
+import pondui.ui.nav.Scaffold
+import pondui.ui.theme.Pond
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun ExampleProfileScreen(
+    route: ExampleProfileRoute,
+    viewModel: ExampleProfileModel = viewModel { ExampleProfileModel(route) }
+) {
+    val state by viewModel.state.collectAsState()
+    val example = state.example
+    if (example == null) return
+    Scaffold {
+        Controls {
+            if (state.isEditing) {
+                TextField(state.symtrix, onTextChange = viewModel::setSymtrix)
+                Button("Done", onClick = viewModel::finalizeEdit)
+            } else {
+                Text(example.symtrix)
+                Button("Edit", onClick = viewModel::toggleEdit, background = Pond.colors.secondary)
+            }
+        }
+    }
+}
